7ced7b37773e7d8db4ac7b894a702d852698a723
Daniel Palmer <daniel@0x0f.com>
2019-12-09 22:46:38 +0900

    gop bits

--- drivers/gpu/drm/mstar/mstar_gop.c
+++ drivers/gpu/drm/mstar/mstar_gop.c
@@ -1,9 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <linux/component.h>
+#include <linux/clk.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/of_device.h>
+#include <linux/regmap.h>
+#include <linux/delay.h>
+#include <linux/interrupt.h>
+#include <linux/of_irq.h>
 
 #include "mstar_gop.h"
 
@@ -11,12 +16,53 @@
 
 struct mstar_gop {
 	struct device *dev;
+	struct regmap *regmap;
+	struct clk *fclk; /* vendor code says this is only needed when setting the palette */
+
+	struct regmap_field *dst;
 };
+
+static const struct regmap_config mstar_gop_regmap_config = {
+	.reg_bits = 16,
+	.val_bits = 16,
+	.reg_stride = 4,
+};
+
+static irqreturn_t mstar_gop_irq(int irq, void *data)
+{
+	return IRQ_HANDLED;
+}
+
+static const char* dsts[] = {
+		"ip_main", "ip_sub", "op", "mvop", "sub_mvop", "unknown", "frc", "unknown"
+};
+
+static void mstar_gop_dump(struct mstar_gop *gop){
+	unsigned int val;
+	regmap_field_read(gop->dst, &val);
+	dev_info(gop->dev, "dst: %s", dsts[val]);
+}
+
+
+static void mstar_gop_reset(struct mstar_gop *gop){
+	dev_info(gop->dev, "reset");
+	regmap_update_bits(gop->regmap, MSTAR_GOP_REG_CONFIG,
+			MSTAR_GOP_REG_CONFIG_RST, MSTAR_GOP_REG_CONFIG_RST);
+	mdelay(10);
+	regmap_update_bits(gop->regmap, MSTAR_GOP_REG_CONFIG,
+			MSTAR_GOP_REG_CONFIG_RST, 0);
+
+	mstar_gop_dump(gop);
+}
 
 static int mstar_gop_bind(struct device *dev, struct device *master,
 			 void *data)
 {
 	struct mstar_gop *gop;
+	struct resource *res;
+	__iomem void *regs;
+	struct platform_device *pdev = to_platform_device(dev);
+	int irq, ret;
 
 	dev_info(dev, "binding");
 
@@ -26,7 +72,41 @@ static int mstar_gop_bind(struct device *dev, struct device *master,
 	}
 
 	gop->dev = dev;
-	return 0;
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if(IS_ERR(res))
+		return PTR_ERR(res);
+
+	regs = devm_ioremap_resource(dev, res);
+	if(IS_ERR(regs))
+		return PTR_ERR(regs);
+
+	gop->regmap = devm_regmap_init_mmio(dev, regs, &mstar_gop_regmap_config);
+	if(IS_ERR(gop->regmap))
+		return PTR_ERR(gop->regmap);
+
+	gop->dst = regmap_field_alloc(gop->regmap, gop_dst_field);
+
+	irq = irq_of_parse_and_map(pdev->dev.of_node, 0);
+	if (!irq){
+		dev_err(gop->dev, "no interrupt provided");
+		return -ENODEV;
+	}
+	else {
+		ret = devm_request_irq(&pdev->dev, irq, mstar_gop_irq, IRQF_SHARED,
+			dev_name(&pdev->dev), gop);
+		if (ret)
+			return ret;
+	}
+
+	gop->fclk = of_clk_get(pdev->dev.of_node, 0);
+	if (IS_ERR(gop->fclk)) {
+		return PTR_ERR(gop->fclk);
+	}
+
+	mstar_gop_reset(gop);
+
+	return ret;
 }
 
 static void mstar_gop_unbind(struct device *dev, struct device *master,
