7ec49b3b22b0ce2174dbf2f1cb9010e2a3a78ee9
Daniel Palmer <daniel@0x0f.com>
2020-04-21 22:17:05 +0900

    Mstar DRM bits

---
+++ drivers/gpu/drm/mstar/mstar_pnl.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * 0x04 - Vertical sync start
+ * 0x08 - Vertical sync end
+ * 0x0c - vfde? start
+ * 0x10 - vfde? end
+ * 0x14 - vde? start
+ * 0x18 - vde? end
+ * 0x1c - vtt?
+ * 0x20 - horizontal sync start
+ * 0x24 - horizontal sync end
+ * 0x28 - hfde? start
+ * 0x2c - hfde? end
+ * 0x30 - hde? start
+ * 0x34 - hde? end
+ * 0x38 - htt
+ * 0x3c - frame color en?
+ * 0x40 - frame color
+ *
+ * 0x180 - src sel?
+ *
+ * |  0
+ * | gop
+ */
