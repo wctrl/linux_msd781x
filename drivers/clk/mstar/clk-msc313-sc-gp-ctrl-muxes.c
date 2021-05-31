932a3f53a95bec9e30414732ca3ddc1b50c62795
Daniel Palmer <daniel@0x0f.com>
2021-05-31 19:27:22 +0900

    sc gp clocks

--- drivers/clk/mstar/clk-msc313-sc-gp-ctrl-muxes.c
+++ drivers/clk/mstar/clk-msc313-sc-gp-ctrl-muxes.c
@@ -10,25 +10,42 @@
 
 #include "clk-msc313-mux.h"
 
-static const struct clk_parent_data emac1_rxtx_parents[] = {
+static const struct clk_parent_data emac_rxtx_parents[] = {
 	{ .fw_name = "eth_buf" },
 	{ .fw_name = "rmii_buf" },
 };
 
-static const struct clk_parent_data emac1_rxtx_ref_parents[] = {
+static const struct clk_parent_data emac_rxtx_ref_parents[] = {
 	{ .fw_name = "rmii_buf" },
 };
 
+#define EMAC_MUXES \
+	MSC313_MUX_DATA("emac_rx", emac_rxtx_parents, 0x88, 0, 2, 1, -1), \
+	MSC313_MUX_DATA("emac_rx_ref", emac_rxtx_ref_parents, 0x88, 8, 10, 1, -1), \
+	MSC313_MUX_DATA("emac_tx", emac_rxtx_parents, 0x8c, 0, 2, 1, -1), \
+	MSC313_MUX_DATA("emac_tx_ref", emac_rxtx_ref_parents, 0x8c, 8, 10, 1, -1)
+
+static const struct msc313_mux_data msc313_muxes[] = {
+	EMAC_MUXES,
+};
+
+static const struct msc313_muxes_data msc313_data = MSC313_MUXES_DATA(msc313_muxes);
+
 static const struct msc313_mux_data ssd20xd_muxes[] = {
-	MSC313_MUX_DATA("emac1_rx", emac1_rxtx_parents, 0xcc, 0, 2, 1, -1),
-	MSC313_MUX_DATA("emac1_rx_ref", emac1_rxtx_ref_parents, 0xcc, 8, 10, 1, -1),
-	MSC313_MUX_DATA("emac1_tx", emac1_rxtx_parents, 0xd0, 0, 2, 1, -1),
-	MSC313_MUX_DATA("emac1_tx_ref", emac1_rxtx_ref_parents, 0xd0, 8, 10, 1, -1),
+	EMAC_MUXES,
+	MSC313_MUX_DATA("emac1_rx", emac_rxtx_parents, 0xcc, 0, 2, 1, -1),
+	MSC313_MUX_DATA("emac1_rx_ref", emac_rxtx_ref_parents, 0xcc, 8, 10, 1, -1),
+	MSC313_MUX_DATA("emac1_tx", emac_rxtx_parents, 0xd0, 0, 2, 1, -1),
+	MSC313_MUX_DATA("emac1_tx_ref", emac_rxtx_ref_parents, 0xd0, 8, 10, 1, -1),
 };
 
 static const struct msc313_muxes_data ssd20xd_data = MSC313_MUXES_DATA(ssd20xd_muxes);
 
 static const struct of_device_id msc313e_sc_gp_ctrl_muxes_of_match[] = {
+	{
+		.compatible = "mstar,msc313-sc-gp-ctrl-muxes",
+		.data = &msc313_data,
+	},
 	{
 		.compatible = "sstar,ssd20xd-sc-gp-ctrl-muxes",
 		.data = &ssd20xd_data,
