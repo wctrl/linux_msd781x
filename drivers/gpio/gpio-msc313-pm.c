0576bd70a270518c13bab3ea9cd176cdffda161b
Daniel Palmer <daniel@0x0f.com>
2021-08-30 20:40:52 +0900

    gpio: mstar: pm: Add compat for ssd212

--- drivers/gpio/gpio-msc313-pm.c
+++ drivers/gpio/gpio-msc313-pm.c
@@ -327,6 +327,13 @@ static const struct of_device_id msc313_pm_gpio_of_match[] = {
 		.data		= &info_msc313,
 	},
 #endif
+//#ifdef CONFIG_MACH_PIONEER
+	{
+		/* SSD212 */
+		.compatible	= "sstar,ssd212-gpio-pm",
+		.data		= &info_msc313,
+	},
+//#endif
 #ifdef CONFIG_MACH_MERCURY
 	{
 		/* SSC8336, SSC8336N */
