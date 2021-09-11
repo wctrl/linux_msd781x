a38d4abd1180045c0bcc033ead94c04d846c10be
Daniel Palmer <daniel@0x0f.com>
2021-09-11 23:28:48 +0900

    serial: 8250_dw: Mark acpi match table as maybe unused
    When building kernels without ACPI support the table is declared
    but is not used because ACPI_PTR() turns it into a NULL.
    
    Add the __maybe_unused attribute to stop the compiler whining.
    
    Signed-off-by: Daniel Palmer <daniel@0x0f.com>

--- drivers/tty/serial/8250/8250_dw.c
+++ drivers/tty/serial/8250/8250_dw.c
@@ -700,7 +700,7 @@ static const struct of_device_id dw8250_of_match[] = {
 };
 MODULE_DEVICE_TABLE(of, dw8250_of_match);
 
-static const struct acpi_device_id dw8250_acpi_match[] = {
+static const struct acpi_device_id __maybe_unused dw8250_acpi_match[] = {
 	{ "INT33C4", 0 },
 	{ "INT33C5", 0 },
 	{ "INT3434", 0 },
