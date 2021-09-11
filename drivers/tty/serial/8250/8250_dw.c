4c79c5c641072d880ccc4706661bb55b49bfa024
Daniel Palmer <daniel@0x0f.com>
2021-09-11 23:35:39 +0900

    serial: 8250_dw: Reduce useless compatibles
    Reduce 96 bytes
    
       text    data     bss     dec     hex filename
    8014346 2949320  249984 11213650         ab1b52 linux/vmlinux
    
       text    data     bss     dec     hex filename
    8014346 2945224  249984 11209554         ab0b52 linux/vmlinux

--- drivers/tty/serial/8250/8250_dw.c
+++ drivers/tty/serial/8250/8250_dw.c
@@ -699,10 +699,12 @@ static const struct dev_pm_ops dw8250_pm_ops = {
 
 static const struct of_device_id dw8250_of_match[] = {
 	{ .compatible = "snps,dw-apb-uart" },
+#if 0
 	{ .compatible = "cavium,octeon-3860-uart" },
 	{ .compatible = "marvell,armada-38x-uart" },
 	{ .compatible = "renesas,rzn1-uart" },
 	{ .compatible = "starfive,jh7100-uart" },
+#endif
 	{ .compatible = "mstar,msc313-uart" },
 	{ /* Sentinel */ }
 };
