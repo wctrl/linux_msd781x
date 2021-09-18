6fcbfb87f7377de711cfb6a7987c184cf5c077cd
Daniel Palmer <daniel@0x0f.com>
2021-09-19 00:50:09 +0900

    include/dt-bindings/gpio/mstar.h to be removed

---
+++ include/dt-bindings/gpio/mstar.h
@@ -0,0 +1,12 @@
+#ifndef _DT_BINDINGS_GPIO_MSTAR_H
+#define _DT_BINDINGS_GPIO_MSTAR_H
+
+#define SSC8336_UNKNOWN_0	0
+#define SSC8333_UNKNOWN_1	1
+#define SSC8336_FUART_RX	2
+#define SSC8333_FUART_TX	3
+
+#define PM_GPIO4_IRQ	6
+#define PM_GPIO5_IRQ	7
+
+#endif
