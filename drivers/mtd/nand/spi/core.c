52f00a2771e8bc10122603475c8c367d2554df0a
Daniel Palmer <daniel@0x0f.com>
2021-12-12 17:09:38 +0900

    mtd: spinand: Support for xincun

--- drivers/mtd/nand/spi/core.c
+++ drivers/mtd/nand/spi/core.c
@@ -949,6 +949,7 @@ static const struct spinand_manufacturer *spinand_manufacturers[] = {
 	&toshiba_spinand_manufacturer,
 	&winbond_spinand_manufacturer,
 	&xtx_spinand_manufacturer,
+	&xincun_spinand_manufacturer,
 };
 
 static int spinand_manufacturer_match(struct spinand_device *spinand,
