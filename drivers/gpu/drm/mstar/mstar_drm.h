2d214078f06a6dd1b99256130079cd46bf310a44
Daniel Palmer <daniel@0x0f.com>
2022-01-20 21:21:00 +0900

    drm top level

--- drivers/gpu/drm/mstar/mstar_drm.h
+++ drivers/gpu/drm/mstar/mstar_drm.h
@@ -3,13 +3,11 @@
 #ifndef _MSTAR_DRM_H_
 #define _MSTAR_MSTAR_DRM_H_
 
-struct mstar_ge;
 struct mstar_top;
 
 struct mstar_drv {
 	struct device *dev;
 	struct mstar_top* top;
-	struct mstar_ge* ge;
 };
 
 #endif /* _MSTAR_DRM_H_ */
