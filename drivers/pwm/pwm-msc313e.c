848fa237b4f9b398da7c216c150e089737eef548
Romain Perier <romain.perier@gmail.com>
2022-01-21 21:07:37 +0100

    pwm: msc313e: fixes

--- drivers/pwm/pwm-msc313e.c
+++ drivers/pwm/pwm-msc313e.c
@@ -1,22 +1,20 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2021 Daniel Palmer <daniel@thingy.jp>
+ * Copyright (C) 2022 Romain Perier <romain.perier@gmail.com>
  */
 
-#include <linux/module.h>
-#include <linux/of.h>
-#include <linux/of_device.h>
-#include <linux/platform_device.h>
-#include <linux/regmap.h>
 #include <linux/clk.h>
+#include <linux/of_device.h>
 #include <linux/pwm.h>
+#include <linux/regmap.h>
 
 #define DRIVER_NAME "msc313e-pwm"
 
 #define CHANNEL_OFFSET	0x80
 #define REG_DUTY	0x8
 #define REG_PERIOD	0x10
-#define REG_DIV  	0x18
+#define REG_DIV		0x18
 #define REG_CTRL	0x1c
 
 struct msc313e_pwm_channel {
@@ -55,27 +53,15 @@ static const struct msc313e_pwm_info ssd20xd_data = {
 	.channels = 4,
 };
 
-static const struct of_device_id msc313e_pwm_dt_ids[] = {
-	{
-		.compatible = "mstar,msc313e-pwm",
-		.data = &msc313e_data,
-	},
-	{
-		.compatible = "mstar,ssd20xd-pwm",
-		.data = &ssd20xd_data,
-	},
-	{},
-};
-MODULE_DEVICE_TABLE(of, msc313e_pwm_dt_ids);
 
-static void msc313e_pwm_writecounter(struct regmap_field* low, struct regmap_field* high, u32 value)
+static void msc313e_pwm_writecounter(struct regmap_field *low, struct regmap_field *high, u32 value)
 {
 	regmap_field_write(low, value);
 	regmap_field_write(high, value >> 16);
 }
 
 static int msc313e_pwm_config(struct pwm_chip *chip, struct pwm_device *device,
-		      int duty_ns, int period_ns)
+			      int duty_ns, int period_ns)
 {
 	struct msc313e_pwm *pwm = to_msc313e_pwm(chip);
 	struct msc313e_pwm_channel *channel = &pwm->channels[device->hwpwm];
@@ -83,9 +69,10 @@ static int msc313e_pwm_config(struct pwm_chip *chip, struct pwm_device *device,
 	unsigned long long div = 1;
 
 	/* fit the period into the period register by prescaling the clk */
-	while(DIV_ROUND_CLOSEST_ULL(period_ns, (nspertick = DIV_ROUND_CLOSEST_ULL(nspertick, div))) > 0x3ffff){
+	while (DIV_ROUND_CLOSEST_ULL(period_ns, (nspertick = DIV_ROUND_CLOSEST_ULL(nspertick, div)))
+	      > 0x3ffff){
 		div++;
-		if(div > (0xffff + 1)){
+		if (div > (0xffff + 1)) {
 			dev_err(chip->dev, "Can't fit period into period register\n");
 			return -EINVAL;
 		}
@@ -93,21 +80,21 @@ static int msc313e_pwm_config(struct pwm_chip *chip, struct pwm_device *device,
 
 	regmap_field_write(channel->clkdiv, div - 1);
 	msc313e_pwm_writecounter(channel->dutyl, channel->dutyh,
-			DIV_ROUND_CLOSEST_ULL(duty_ns, nspertick));
+				 DIV_ROUND_CLOSEST_ULL(duty_ns, nspertick));
 	msc313e_pwm_writecounter(channel->periodl, channel->periodh,
-			DIV_ROUND_CLOSEST_ULL(period_ns, nspertick));
+				 DIV_ROUND_CLOSEST_ULL(period_ns, nspertick));
 
 	return 0;
 };
 
 static int msc313e_pwm_set_polarity(struct pwm_chip *chip, struct pwm_device *device,
-			    enum pwm_polarity polarity)
+				    enum pwm_polarity polarity)
 {
 	struct msc313e_pwm *pwm = to_msc313e_pwm(chip);
 	struct msc313e_pwm_channel *channel = &pwm->channels[device->hwpwm];
-	unsigned pol = 0;
+	unsigned int pol = 0;
 
-	if(polarity == PWM_POLARITY_INVERSED)
+	if (polarity == PWM_POLARITY_INVERSED)
 		pol = 1;
 	regmap_field_update_bits(channel->polarity, 1, pol);
 
@@ -129,7 +116,7 @@ static void msc313e_pwm_disable(struct pwm_chip *chip, struct pwm_device *device
 }
 
 static int msc313e_apply(struct pwm_chip *chip, struct pwm_device *pwm,
-		     const struct pwm_state *state)
+			 const struct pwm_state *state)
 {
 	msc313e_pwm_set_polarity(chip, pwm, state->polarity);
 	msc313e_pwm_config(chip, pwm, state->duty_cycle, state->period);
@@ -138,10 +125,11 @@ static int msc313e_apply(struct pwm_chip *chip, struct pwm_device *pwm,
 }
 
 static void msc313e_get_state(struct pwm_chip *chip, struct pwm_device *device,
-			  struct pwm_state *state){
+			      struct pwm_state *state)
+{
 	struct msc313e_pwm *pwm = to_msc313e_pwm(chip);
 	struct msc313e_pwm_channel *channel = &pwm->channels[device->hwpwm];
-	unsigned pol = 0;
+	unsigned int pol = 0;
 
 	regmap_field_read(channel->polarity, &pol);
 	state->polarity = pol;
@@ -159,11 +147,11 @@ static const struct pwm_ops msc313e_pwm_ops = {
 
 static int msc313e_pwm_probe(struct platform_device *pdev)
 {
-	int ret, i;
-	__iomem void* base;
-	struct msc313e_pwm *pwm;
-	struct device *dev = &pdev->dev;
 	const struct msc313e_pwm_info *match_data;
+	struct device *dev = &pdev->dev;
+	struct msc313e_pwm *pwm;
+	__iomem void *base;
+	int i;
 
 	match_data = of_device_get_match_data(dev);
 	if (!match_data)
@@ -177,12 +165,13 @@ static int msc313e_pwm_probe(struct platform_device *pdev)
 	if (!pwm)
 		return -ENOMEM;
 
-	pwm->clk = of_clk_get(dev->of_node, 0);
+	pwm->clk = devm_clk_get(dev, NULL);
+	if (IS_ERR(pwm->clk))
+		return dev_err_probe(dev, PTR_ERR(pwm->clk), "Cannot get clk\n");
 
-	pwm->regmap = devm_regmap_init_mmio(dev, base,
-			&msc313e_pwm_regmap_config);
-	if(IS_ERR(pwm->regmap))
-		return PTR_ERR(pwm->regmap);
+	pwm->regmap = devm_regmap_init_mmio(dev, base, &msc313e_pwm_regmap_config);
+	if (IS_ERR(pwm->regmap))
+		return dev_err_probe(dev, PTR_ERR(pwm->regmap), "Cannot get regmap\n");
 
 	for (i = 0; i < match_data->channels; i++) {
 		unsigned int offset = CHANNEL_OFFSET * i;
@@ -193,8 +182,10 @@ static int msc313e_pwm_probe(struct platform_device *pdev)
 		struct reg_field periodl_field = REG_FIELD(offset + REG_PERIOD, 0, 15);
 		struct reg_field periodh_field = REG_FIELD(offset + REG_PERIOD + 4, 0, 2);
 
-		pwm->channels[i].clkdiv = devm_regmap_field_alloc(dev, pwm->regmap, div_clkdiv_field);
-		pwm->channels[i].polarity = devm_regmap_field_alloc(dev, pwm->regmap, ctrl_polarity_field);
+		pwm->channels[i].clkdiv = devm_regmap_field_alloc(dev, pwm->regmap,
+								  div_clkdiv_field);
+		pwm->channels[i].polarity = devm_regmap_field_alloc(dev, pwm->regmap,
+								    ctrl_polarity_field);
 		pwm->channels[i].dutyl = devm_regmap_field_alloc(dev, pwm->regmap, dutyl_field);
 		pwm->channels[i].dutyh = devm_regmap_field_alloc(dev, pwm->regmap, dutyh_field);
 		pwm->channels[i].periodl = devm_regmap_field_alloc(dev, pwm->regmap, periodl_field);
@@ -208,27 +199,20 @@ static int msc313e_pwm_probe(struct platform_device *pdev)
 	pwm->pwmchip.of_xlate = of_pwm_xlate_with_flags;
 	pwm->pwmchip.of_pwm_n_cells = 3;
 
-	ret = pwmchip_add(&pwm->pwmchip);
-	if(ret)
-		return ret;
-
 	platform_set_drvdata(pdev, pwm);
 
-	return 0;
+	return devm_pwmchip_add(dev, &pwm->pwmchip);
 }
 
-static int msc313e_pwm_remove(struct platform_device *pdev)
-{
-	struct msc313e_pwm *pwm = platform_get_drvdata(pdev);
-
-	pwmchip_remove(&pwm->pwmchip);
-
-	return 0;
-}
+static const struct of_device_id msc313e_pwm_dt_ids[] = {
+	{ .compatible = "mstar,msc313e-pwm", .data = &msc313e_data },
+	{ .compatible = "mstar,ssd20xd-pwm", .data = &ssd20xd_data },
+	{},
+};
+MODULE_DEVICE_TABLE(of, msc313e_pwm_dt_ids);
 
 static struct platform_driver msc313e_pwm_driver = {
 	.probe = msc313e_pwm_probe,
-	.remove = msc313e_pwm_remove,
 	.driver = {
 		.name = DRIVER_NAME,
 		.of_match_table = msc313e_pwm_dt_ids,
