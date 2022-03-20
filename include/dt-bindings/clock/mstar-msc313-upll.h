0469b54ed79d70b45f5f3d9cd8fc2563b4b55629
Daniel Palmer <daniel@0x0f.com>
2022-03-20 13:01:11 +0900

    dt-bindings: clk: mstar msc313 upll binding header

---
+++ include/dt-bindings/clock/mstar-msc313-upll.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+/*
+ * Output definitions for the MStar/SigmaStar MPLL
+ *
+ * Copyright (C) 2022 Daniel Palmer <daniel@thingy.jp>
+ */
+
+#ifndef _DT_BINDINGS_CLOCK_MSTAR_MSC313_UPLL_H
+#define _DT_BINDINGS_CLOCK_MSTAR_MSC313_UPLL_H
+
+#define MSTAR_MSC313_UPLL_384	0
+#define MSTAR_MSC313_UPLL_320	1
+
+#endif
