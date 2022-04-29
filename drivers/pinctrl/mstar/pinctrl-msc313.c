06b93e8ecbf774f174ed39a81e3e82991f92c7c8
Daniel Palmer <daniel@0x0f.com>
2022-04-29 11:58:34 +0900

    pinctrl: ssd203d

--- drivers/pinctrl/mstar/pinctrl-msc313.c
+++ drivers/pinctrl/mstar/pinctrl-msc313.c
@@ -1069,6 +1069,67 @@ static const struct msc313_pinctrl_pinconf ssd20xd_configurable_pins[] = {
 };
 
 MSTAR_PINCTRL_INFO(ssd20xd);
+
+#define SSD203D_COMMON_PIN(_pinname) COMMON_PIN(SSD203D, _pinname)
+
+static const struct pinctrl_pin_desc ssd203d_pins[] = {
+	SSD20XD_COMMON_PIN(GPIO85),
+	SSD20XD_COMMON_PIN(GPIO86),
+	SSD203D_COMMON_PIN(HDMITX_SCL),
+	SSD203D_COMMON_PIN(HDMITX_SDA),
+	SSD203D_COMMON_PIN(HDMITXHPD),
+	SSD203D_COMMON_PIN(PM_SPI_CZ),
+	SSD203D_COMMON_PIN(PM_SPI_CK),
+	SSD203D_COMMON_PIN(PM_SPI_DI),
+	SSD203D_COMMON_PIN(PM_SPI_DO),
+	SSD203D_COMMON_PIN(PM_SPI_HLD),
+	SSD203D_COMMON_PIN(PM_SPI_WPZ),
+	SSD203D_COMMON_PIN(PM_IRIN),
+	SSD203D_COMMON_PIN(PM_UART_RX),
+	SSD203D_COMMON_PIN(PM_UART_TX),
+	SSD203D_COMMON_PIN(HDMI2TXCN),
+	SSD203D_COMMON_PIN(HDMI2TXCP),
+	SSD203D_COMMON_PIN(HDMI2TX0N),
+	SSD203D_COMMON_PIN(HDMI2TX0P),
+	SSD203D_COMMON_PIN(HDMI2TX1N),
+	SSD203D_COMMON_PIN(HDMI2TX1P),
+	SSD203D_COMMON_PIN(HDMI2TX2N),
+	SSD203D_COMMON_PIN(HDMI2TX2P),
+};
+
+/* i2c0 */
+static const int ssd203d_i2c0_mode1_pins[] = {
+	PIN_SSD203D_HDMITX_SCL,
+	PIN_SSD203D_HDMITX_SCL,
+};
+
+static const char * const ssd203d_i2c0_groups[] = {
+	GROUPNAME_I2C0_MODE1,
+};
+
+static const u16 ssd203d_i2c0_values[] = {
+	SSD20XD_MODE(I2C0, 1),
+};
+
+#define SSD203D_PINCTRL_GROUP(_NAME, _name) \
+	MSTAR_PINCTRL_GROUP(GROUPNAME_##_NAME, ssd203d_##_name##_pins)
+
+static const struct msc313_pinctrl_group ssd203d_pinctrl_groups[] = {
+	SSD203D_PINCTRL_GROUP(I2C0_MODE1, i2c0_mode1),
+};
+
+#define SSD203D_FUNCTION(_NAME, _name) \
+	MSTAR_PINCTRL_FUNCTION(FUNCTIONNAME_##_NAME, REG_SSD20XD_##_NAME, \
+	MASK_SSD20XD_##_NAME, ssd203d_##_name##_groups, ssd203d_##_name##_values)
+
+static const struct msc313_pinctrl_function ssd203d_pinctrl_functions[] = {
+	SSD203D_FUNCTION(I2C0, i2c0),
+};
+
+static const struct msc313_pinctrl_pinconf ssd203d_configurable_pins[] = {
+};
+
+MSTAR_PINCTRL_INFO(ssd203d);
 #endif /* infinity */
 
 #ifdef CONFIG_MACH_MERCURY
@@ -1552,6 +1613,10 @@ static const struct of_device_id msc313_pinctrl_of_match[] = {
 	{
 		.compatible	= "sstar,ssd20xd-pinctrl",
 		.data		= &ssd20xd_info,
+	},
+	{
+		.compatible	= "sstar,ssd203d-pinctrl",
+		.data		= &ssd203d_info,
 	},
 #endif
 #ifdef CONFIG_MACH_MERCURY
