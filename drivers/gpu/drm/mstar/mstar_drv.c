b08b0cd2ae3aa50d7c668c7acc025799fa5fd59d
Daniel Palmer <daniel@0x0f.com>
2022-10-09 15:08:11 +0900

    rebase fixes: drm

--- drivers/gpu/drm/mstar/mstar_drv.c
+++ drivers/gpu/drm/mstar/mstar_drv.c
@@ -12,9 +12,9 @@
 
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_drv.h>
-#include <drm/drm_fb_cma_helper.h>
+#include <drm/drm_fb_dma_helper.h>
 #include <drm/drm_fb_helper.h>
-#include <drm/drm_gem_cma_helper.h>
+#include <drm/drm_gem_dma_helper.h>
 #include <drm/drm_of.h>
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_vblank.h>
