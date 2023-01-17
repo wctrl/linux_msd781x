9be5af8f9636adf8c6cb964960bc1eca2c16b758
Daniel Palmer <daniel@0x0f.com>
2023-01-17 19:08:03 +0900

    mstar usb phy

--- drivers/phy/mstar/phy-msc313-usb.c
+++ drivers/phy/mstar/phy-msc313-usb.c
@@ -11,19 +11,19 @@
 #include <linux/platform_device.h>
 #include <linux/mfd/syscon.h>
 #include <linux/regmap.h>
+#include <linux/usb/otg.h>
 #include <linux/usb/mstar_usbc.h>
 #include <linux/usb/mstar_utmi.h>
 #include <linux/regulator/consumer.h>
 
-#define NUM_PORTS 2
-#define PORT_UHC 0
-#define PORT_OTG 1
-
 struct msc313_usb_phy {
 	struct device *dev;
-	struct phy *ports[NUM_PORTS];
 	struct regmap *utmi;
 	struct regmap *usbc;
+	struct regmap *bc;
+
+	enum usb_dr_mode dr_mode;
+
 	struct regulator *vbus;
 };
 
@@ -43,47 +43,47 @@ static const struct phy_ops msc313_usb_phy_ops = {
 	.owner = THIS_MODULE,
 };
 
-static struct phy *msc313_usb_phy_xlate(struct device *dev,
-					struct of_phandle_args *args)
-{
-	struct msc313_usb_phy *msc313_usb_phy = dev_get_drvdata(dev);
-	int port = args->args[0];
-
-	printk("getting port %d\n", port);
-
-	if (!msc313_usb_phy)
-		return  ERR_PTR(-ENODEV);
-	if(port >= NUM_PORTS)
-		return ERR_PTR(-ENODEV);
-
-	return msc313_usb_phy->ports[port];
-}
-
 static irqreturn_t msc313_usb_phy_irq(int irq, void *data)
 {
-        struct msc313_usb_phy *phy = data;
-	printk("int\n");
+	struct msc313_usb_phy *phy = data;
+
+	dev_info(phy->dev, "int\n");
 	regmap_write(phy->usbc, MSTAR_USBC_REG_INTSTS, MSTAR_USBC_INT_MASK);
+
 	return IRQ_HANDLED;
 }
 
 static void msc313_usb_phy_switch_port(struct msc313_usb_phy *phy)
 {
-	dev_info(phy->dev, "Switching to UHC port\n");
 	regmap_update_bits(phy->usbc, MSTAR_USBC_REG_PRTCTRL,
 				MSTAR_PRTCTRL_OTG | MSTAR_PRTCTRL_UHC, 0);
-	regmap_update_bits(phy->usbc, MSTAR_USBC_REG_PRTCTRL,
-				MSTAR_PRTCTRL_UHC, MSTAR_PRTCTRL_UHC);
+
+	switch(phy->dr_mode){
+	case USB_DR_MODE_HOST:
+		dev_info(phy->dev, "Switching port to UHC\n");
+		regmap_update_bits(phy->usbc, MSTAR_USBC_REG_PRTCTRL,
+					      MSTAR_PRTCTRL_UHC, MSTAR_PRTCTRL_UHC);
+		break;
+	case USB_DR_MODE_PERIPHERAL:
+		dev_info(phy->dev, "Switching port to OTG\n");
+		regmap_update_bits(phy->usbc, MSTAR_USBC_REG_PRTCTRL,
+					      MSTAR_PRTCTRL_OTG, MSTAR_PRTCTRL_OTG);
+		break;
+	default:
+		dev_info(phy->dev, "Disconnecting port\n");
+		break;
+	}
+
 	// not sure what this is, might be MAC power down
 	//regmap_update_bits(msc313_usb_phy->usbc, MSTAR_USBC_REG_RSTCTRL,
 	//			BIT(MSTAR_RSTCTRL_VBUSVALID),
 	//			0);
-
 }
 
 static void msc313_usb_phy_do_calibration(struct msc313_usb_phy *phy)
 {
 	unsigned int calval;
+
 	dev_info(phy->dev, "starting calibration...\n");
 	regmap_update_bits(phy->utmi, MSTAR_UTMI_REG_CAL,
 			MSTAR_UTMI_REG_CAL_START, MSTAR_UTMI_REG_CAL_START);
@@ -106,9 +106,11 @@ static void msc313_usb_phy_do_calibration(struct msc313_usb_phy *phy)
 static int msc313_usb_phy_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	struct phy_provider *phy_provider;
 	struct msc313_usb_phy *msc313_usb_phy;
-	int i, irq;
+	struct device_node *np = dev->of_node;
+	struct phy_provider *phy_provider;
+	struct phy *phy;
+	int irq, ret;
 	bool swap;
 
 	msc313_usb_phy = devm_kzalloc(dev, sizeof(*msc313_usb_phy), GFP_KERNEL);
@@ -118,13 +120,21 @@ static int msc313_usb_phy_probe(struct platform_device *pdev)
 	msc313_usb_phy->dev = dev;
 
 	msc313_usb_phy->utmi = syscon_regmap_lookup_by_phandle(dev->of_node, "mstar,utmi");
-	if(IS_ERR(msc313_usb_phy->utmi)){
+	if (IS_ERR(msc313_usb_phy->utmi)) {
 		return PTR_ERR(msc313_usb_phy->utmi);
 	}
 
 	msc313_usb_phy->usbc = syscon_regmap_lookup_by_phandle(dev->of_node, "mstar,usbc");
 	if (IS_ERR(msc313_usb_phy->usbc))
 		return PTR_ERR(msc313_usb_phy->usbc);
+
+	msc313_usb_phy->bc = syscon_regmap_lookup_by_phandle(dev->of_node, "mstar,bc");
+	if (IS_ERR(msc313_usb_phy->bc)) {
+		msc313_usb_phy->bc = NULL;
+	}
+
+	//msc313_usb_phy->dr_mode = usb_get_dr_mode(dev);
+	msc313_usb_phy->dr_mode = USB_DR_MODE_HOST;
 
 	irq = irq_of_parse_and_map(pdev->dev.of_node, 0);
 	if (!irq){
@@ -153,12 +163,15 @@ static int msc313_usb_phy_probe(struct platform_device *pdev)
 	// clear any existing interrupts and then enable
 	// the interrupt
 	regmap_write(msc313_usb_phy->usbc, MSTAR_USBC_REG_INTEN,
-			MSTAR_USBC_INT_MASK);
+		     MSTAR_USBC_INT_MASK);
 	regmap_write(msc313_usb_phy->usbc, MSTAR_USBC_REG_INTSTS,
-                        MSTAR_USBC_INT_MASK);
-	if(irq){
-		devm_request_irq(&pdev->dev, irq, msc313_usb_phy_irq, IRQF_SHARED,
+		     MSTAR_USBC_INT_MASK);
+	if (irq) {
+		ret = devm_request_irq(&pdev->dev, irq, msc313_usb_phy_irq, IRQF_SHARED,
 				dev_name(&pdev->dev), msc313_usb_phy);
+
+		if (ret)
+			return ret;
 	}
 
 	// power up hacks
@@ -176,7 +189,6 @@ static int msc313_usb_phy_probe(struct platform_device *pdev)
 	regmap_write(msc313_usb_phy->utmi, REG_EYESETTING1, 0x0210);
 	regmap_write(msc313_usb_phy->utmi, REG_EYESETTING2, 0x8100);
 
-
 	msc313_usb_phy_do_calibration(msc313_usb_phy);
 
 	swap = of_property_read_bool(dev->of_node, "mstar,utmi-dxswap");
@@ -188,13 +200,10 @@ static int msc313_usb_phy_probe(struct platform_device *pdev)
 
 	//regmap_update_bits(msc313_usb_phy->regmap, REG_PWRCTRL, PWRCTRL_UPLL_PDN, 0);
 
-	for(i = 0; i < NUM_PORTS; i++){
-		msc313_usb_phy->ports[i] = devm_phy_create(dev, NULL, &msc313_usb_phy_ops);
-		if (IS_ERR(msc313_usb_phy->ports[i])) {
-			return PTR_ERR(msc313_usb_phy->ports[i]);
-		}
-		phy_set_drvdata(msc313_usb_phy->ports[i], msc313_usb_phy);
-	}
+	phy = devm_phy_create(dev, NULL, &msc313_usb_phy_ops);
+	if (IS_ERR(phy))
+		return PTR_ERR(phy);
+	phy_set_drvdata(phy, msc313_usb_phy);
 
 	msc313_usb_phy->vbus = devm_regulator_get(dev, "vbus");
 	regulator_enable(msc313_usb_phy->vbus);
@@ -202,7 +211,7 @@ static int msc313_usb_phy_probe(struct platform_device *pdev)
 	dev_set_drvdata(dev, msc313_usb_phy);
 
 	phy_provider = devm_of_phy_provider_register(&pdev->dev,
-						     msc313_usb_phy_xlate);
+						     of_phy_simple_xlate);
 	return PTR_ERR_OR_ZERO(phy_provider);
 }
 
